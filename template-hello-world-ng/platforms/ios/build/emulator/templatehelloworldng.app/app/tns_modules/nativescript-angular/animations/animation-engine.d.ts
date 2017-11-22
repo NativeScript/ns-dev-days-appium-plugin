ab39735d3f1e7113c729a63eaa33e688b1af20d5
SvetoslavTsenov <tsenov.svetoslav@gmail.com>
2017-11-22 15:10:13 +0200

    ref: remove platforms ...

--- template-hello-world-ng/platforms/ios/build/emulator/templatehelloworldng.app/app/tns_modules/nativescript-angular/animations/animation-engine.d.ts
+++
@@ -1,5 +0,0 @@
-import { AnimationDriver, ɵAnimationEngine as AnimationEngine } from "@angular/animations/browser";
-import { AnimationStyleNormalizer } from "@angular/animations/browser/src/dsl/style_normalization/animation_style_normalizer";
-export declare class NativeScriptAnimationEngine extends AnimationEngine {
-    constructor(driver: AnimationDriver, normalizer: AnimationStyleNormalizer);
-}
--- template-hello-world-ng/platforms/ios/build/emulator/templatehelloworldng.app/app/tns_modules/nativescript-angular/animations/transition-animation-engine.d.ts
+++
@@ -1,7 +0,0 @@
-import { TransitionAnimationEngine } from "./private-imports/render/transition_animation_engine";
-export declare class NSTransitionAnimationEngine extends TransitionAnimationEngine {
-    flush(microtaskId?: number): void;
-    private _flushAnimationsOverride(microtaskId);
-    elementContainsData(namespaceId: string, element: any): any;
-    private _beforeAnimationBuildOverride(namespaceId, instruction, allPreviousPlayersMap);
-}
